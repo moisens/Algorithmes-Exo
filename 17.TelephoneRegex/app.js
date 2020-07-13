48e6ac241b906f5914fad81fee2664df0a7a8cbe
moise <mose.ns@hotmail.com>
2020-07-13 13:34:58 +0200

    Exo 17

---
+++ 17.TelephoneRegex/app.js
@@ -0,0 +1,25 @@
+function telephoneCheck(str){
+    
+   let reg = /^0[1-9]([-. ]?[0-9]{2}){4}/;
+   if(reg.test(str)){
+      return true;
+   }else{
+      return false;
+   }
+   
+
+}
+
+console.log(telephoneCheck("06 44 64 90 21"));
+
+
+// ----------------- ÉNONCÉ -----------------
+
+// Vous devez vous assurer que vos utilisateurs rentrent bien un numéro Français.
+// Ce numéro doit : 
+// Démarrer par un 0 et être suivi d'un chiffre entre 1-9.
+// Il faut qu'il y est 4 duos de chiffre de 1 à 9 après les deux premiers.
+// Nous accepterons des espaces( ), tirets(-) et points(.) entre les duos de
+// chiffres.
+// Retournez true si la chaine en parametre passe le test(si c'est un numéro valable)
+// et false en cas contraire.
